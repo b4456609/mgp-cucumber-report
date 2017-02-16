79712bd4061fb6569705a71629ed9d166e58df9d
Bernie <b4456609@gmail.com>
2017-02-17 00:09:03 +0800

    request url and save to file

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const reporter = require('cucumber-html-reporter')
 const morgan = require('morgan')
+const fs = require('fs')
+const request = require('request');
 
 var app = express()
 
@@ -10,16 +12,29 @@ app.use(express.static('./build'));
 
 app.get('/', function (req, res) {
   console.log(req.query.url);
-  const options = {
-    theme: 'bootstrap',
-    jsonFile: 'test/cucumber.json',
-    output: 'build/report.html',
-    reportSuiteAsScenarios: true,
-    launchReport: false,
-  };
+  let stream = request
+    .get(req.query.url)
+    .on('error', function (err) {
+      console.log(err)
+    })
+    .pipe(fs.createWriteStream('build/cucumber.json'))
 
-  reporter.generate(options);
-  res.sendFile(path.join(__dirname, './build', 'report.html'));
+  stream.on('finish', function () {
+    console.log("The file was saved!");
+    const options = {
+      theme: 'bootstrap',
+      jsonFile: 'build/cucumber.json',
+      output: 'build/report.html',
+      reportSuiteAsScenarios: true,
+      launchReport: false,
+    };
+
+    reporter.generate(options);
+    res.sendFile(path.join(__dirname, './build', 'report.html'));
+  })
+
+
 })
 
 app.listen(3010);
+console.log("The port listen on 3010");
